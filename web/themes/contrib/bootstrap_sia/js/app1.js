cbcc66fb989f8bde9feec2511161289e0a9f24bd
Marc-André Boivin <marc-andre.boivin@outlook.com>
2018-09-11 14:27:48 -0400

    base sia theme

---
+++ web/themes/contrib/bootstrap_sia/js/app1.js
@@ -0,0 +1,127 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
+  
+{
+    "particles": {
+      "number": {
+        "value": 20,
+        "density": {
+          "enable": true,
+          "value_area": 600
+        }
+      },
+      "color": {
+        "value": "#b2c3c8"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 6
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.3,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 0.5,
+          "opacity_min": 0.008120772123013452,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 20,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 0.5,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 300,
+        "color": "#b2c3c8",
+        "opacity": 0.2,
+        "width": 0
+      },
+      "move": {
+        "enable": true,
+        "speed": 1.5,
+        "direction": "bottom-right",
+        "random": true,
+        "straight": true,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 1200,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 167.83216783216784,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 275.7242757242757,
+          "size": 11.988011988011989,
+          "duration": 2,
+          "opacity": 0.8311688311688312,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+
+);
